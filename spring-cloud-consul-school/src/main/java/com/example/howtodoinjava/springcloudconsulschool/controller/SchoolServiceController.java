d141d32ae4e963fa1b170ac1fdecf3d39ecac42c
marwen.saidi <marwen.saidi@orange.com>
2020-01-05 11:44:23 +0100

    code

---
+++ spring-cloud-consul-school/src/main/java/com/example/howtodoinjava/springcloudconsulschool/controller/SchoolServiceController.java
@@ -0,0 +1,22 @@
+package com.example.howtodoinjava.springcloudconsulschool.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.howtodoinjava.springcloudconsulschool.delegate.StudentServiceDelegate;
+
+@RestController
+public class SchoolServiceController {
+	
+	@Autowired
+	StudentServiceDelegate studentServiceDelegate;
+
+
+	// implement http method getStudent
+	}
+
+	
+}
---
+++ spring-cloud-consul-school/src/main/java/com/example/howtodoinjava/springcloudconsulschool/delegate/StudentServiceDelegate.java
@@ -0,0 +1,35 @@
+package com.example.howtodoinjava.springcloudconsulschool.delegate;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class StudentServiceDelegate {
+	@Autowired
+	RestTemplate restTemplate;
+
+	// implement this method
+	public String callStudentServiceAndGetData(String schoolname) {
+		System.out.println("Consul Demo - Getting School details for " + schoolname);
+
+
+
+
+		System.out.println("Response Received as " + "response" + " -  " + new Date());
+
+		return "School Name -  " + schoolname + " :::  Student Details " + "response" + " -  " + new Date();
+	}
+	
+	@Bean
+	// how load balanced this ?
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
